94c46087757d57926cb0bbc0bcd39abddf2505e5
Jacqueline Palmer <jackiemariep@yahoo.com>
2017-04-22 08:57:19 -0700

    created FriendFinder application

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+var friends = [ 
+{
+  "name" : "Bubbles",
+  "photo" : "http://www.writeups.org/wp-content/uploads/Bubbles-Powerpuff-Girls-Profile-a.jpg",
+  "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+  "name" : "Buttercup",
+  "photo" : "http://ex-vtc.gotdns.com/data/OldSites/PPGWorld/www.ppgworld.com/movie/art/buttercup-7.jpg",
+  "scores" : [4,5,3,5,3,5,5,1,5,1]
+}, {
+  "name" : "Blossom",
+  "photo" : "http://images.all-free-download.com/images/graphiclarge/blossom_012_powerpuff_girls_51873.jpg",
+  "scores" : [3,3,5,5,3,1,5,1,1,5]
+}];
+
+
+module.exports = friends;
+
+
+
+
+
+
+
+
+
+
